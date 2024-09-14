c9836d9c00a48d29496c49183aae68fc310f763c
damingwangzhe <155880206+damingwangzhe@users.noreply.github.com>
2024-09-14 16:53:45 +0800

    今天修改

--- haha.py
+++ haha.py
@@ -2,13 +2,3 @@
 for i in range(10):
     print("你好")
     
-class ClassName(object):
-    """docstring for ClassName."""
-    def __init__(self, arg):
-        super(Class
-        Name, self).__init__()
-    arg
-if __name__ == "__main__":
-    main()
-    print("")
-    print("")
